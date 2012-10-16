e142788fe2f8a27f1ac4febcead1b1564a0116cc
Sven Gothel <sgothel@jausoft.com>
2012-10-16 14:54:11 +0200

    Jake2.sh

--- Jake2.sh
+++ Jake2.sh
@@ -5,6 +5,12 @@ cd `dirname $0`
 # for jogl and joal
 CP=lib/jogamp/gluegen-rt.jar:lib/jogamp/joal.jar:lib/jogamp/jogl-all.jar:dist/lib/jake2.jar
 
+#breaks VM's like avian
+#X_ARGS="-Xmx100M"
+
+# should be redundant (AWT not used anyways), however, due to a bug in avian, it's required
+D0_ARGS="-Djava.awt.headless=false"
+
 #D_ARGS="-Dnewt.debug.Window.MouseEvent"
 #D_ARGS="-Dnewt.debug.Window.KeyEvent"
 #D_ARGS="-Djogl.debug=all"
@@ -21,4 +27,4 @@ CP=lib/jogamp/gluegen-rt.jar:lib/jogamp/joal.jar:lib/jogamp/jogl-all.jar:dist/li
 G_ARGS="+set timeout 12000 +set cl_timeout 12000 +set gl_mode 0 +set vid_ref jogles2"
 #G_ARGS="+set timeout 12000 +set cl_timeout 12000 +set gl_mode 0 +set vid_ref jogldummy"
 
-exec java -Xmx100M -cp $CP $D_ARGS jake2.Jake2 $G_ARGS $* 2>&1 | tee Jake2.log
+exec java $X_ARGS -cp $CP $D0_ARGS $D_ARGS jake2.Jake2 $G_ARGS $* 2>&1 | tee Jake2.log
