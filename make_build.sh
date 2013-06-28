28ed823b1a804145b4dd741807a83570abc139e1
Sven Gothel <sgothel@jausoft.com>
2013-06-28 18:54:56 +0200

    build: Use environment var 'PLUGIN_JAR' and property 'plugin.jar' to define plugin.jar for JavaScript bridge; Use javac debug defaults: source,lines.

--- make_build.sh
+++ make_build.sh
@@ -9,6 +9,9 @@ export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
 
 #    -Djavacdebug=true  \
 #    -Djavacdebuglevel="source,lines,vars" \
+#
+#    -Djavacdebug=false  \
+#    -Djavacdebuglevel="" \
 
 java \
     -Dant.home=lib/ant -cp $CP org.apache.tools.ant.Main -buildfile build.xml $@
